9bbbf8c5ec00036c600c5c68b17adeb7d09fcf9b
nembouetmerlin@gmail.comgit config --global user.name guyzogit config --global user.name nembouetmerlin@gmail.com <nembouetmerlin@gmail.com>
2022-02-19 23:37:34 +0100

    add edit feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,20 +51,18 @@
                         </form>
                         @endif
                         <a class = "inlame-block" href = "{{ route('todo.edit', $todo->id)}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" class ="icon icon-tabler icon-tabler-edit ml-4" width ="36" height ="36" viewBox = "0 0 24 24" stroke-width="1.5"
-                                stroke="#c14638" fill ="none" stroke-linecape = "round" stroke-linejoin="round">
-                                <path stroke = "none" d="M0 0h24v24H0z"/>
-                                <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 -2v-3"/>
-                                <path d="M9 15h318,5 -8,5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3"/>
-                                <line x1="16" y1="5" x2="19" y2="8"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="60" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                                
                             </svg>
+
                         </a>
                         <form action = "{{ route('todo.destroy', $todo->id)}}" method = "POST">
                             @method('DELETE')
                             @csrf 
                         
                         <button class ="border-0 bg-transparent">
-                            <svg xmlns="http://www.w3.org/2000/svg" class ="icon icon-tabler icon-tabler-trash ml-2" width ="36" height ="36" viewBox = "0 0 24 24" stroke-width="1.5"
+                            <svg xmlns="http://www.w3.org/2000/svg" class ="icon icon-tabler icon-tabler-trash ml-2" width ="30" height ="36" viewBox = "0 0 24 24" stroke-width="1.5"
                                 stroke="#c14638" fill ="none" stroke-linecape = "round" stroke-linejoin="round">
                                 <path stroke = "none" d="M0 0h24v24H0z"/>
                                 <line x1="4" y1="7" x2="20" y2="7"/>
